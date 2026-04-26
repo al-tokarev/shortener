6d6ae50f3c2ca0a5fb66798dbacf381222f864dc
Александр Токарев <alexandr@MacBook-Pro-Aleksandr.local>
2026-04-26 19:54:06 +0300

    change TestGetShortenedUrl 2

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -9,7 +9,7 @@ var Options struct {
 
 func RunFlags() {
 	flag.StringVar(&Options.AddrServe, "a", "localhost:8080", "Address server")
-	flag.StringVar(&Options.AddrResp, "b", "localhost:8080", "Address response")
+	flag.StringVar(&Options.AddrResp, "b", "http://localhost:8080", "Address response")
 
 	flag.Parse()
 }
